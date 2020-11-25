e2760cefda07e3e468242f8050ca7e4acb05d3fa
kaistella <59009017+kaistella@users.noreply.github.com>
2020-11-25 20:13:12 +0800

    tied starter choice randomization implemented

--- Data/Scripts/024_Important/001_Personality Test.rb
+++ Data/Scripts/024_Important/001_Personality Test.rb
@@ -28,63 +28,73 @@ def pbPersonalityTest
 # Question 1
 	cmd=pbMessage("Do you like to do things according to plan?",["Definitely! I plan everything meticulously.","I can be pretty spontaneous."])
 	if cmd == 0
-		careful =+ 2
-		serious =+ 1
+		careful += 2
+		serious += 1
 	elsif cmd == 1
-		lax =+ 2
-		quirky =+ 1
+		lax += 2
+		quirky += 1
 	end
-
 # Question 2
 	cmd=pbMessage("Your friend takes a spectacular fall! What do you do?",["Follow after them and help them up.","Call for medical assistance.","Point and laugh. How did they manage that?!"])
 	if cmd == 0
-		mild =+ 2
-		brave =+ 1
+		mild += 2
+		brave += 1
 	elsif cmd == 1
-		hasty =+ 2
-		careful =+ 1
+		hasty += 2
+		careful += 1
+		p careful
 	elsif cmd == 2
-		naughty =+2
-		impish =+1
+		naughty +=2
+		impish +=1
 	end
-
 # Question 3
 	cmd=pbMessage("You're in a play with friends. What kind of role do you prefer?",["The starring role. I want all eyes on me!","The supportive best friend.","The tree in the back."])
 	if cmd == 0
-		sassy =+ 2
-		jolly =+ 1
+		sassy += 2
+		jolly += 1
 	elsif cmd == 1
-		modest =+ 2
-		calm =+ 1
+		modest += 2
+		calm += 1
 	elsif cmd == 2
-		bashful =+2
-		timid =+1
+		bashful +=2
+		timid +=1
 	end
-
 # Question 4
 	cmd=pbMessage("Your friends seem to be having a fun chat out of earshot. What do you do?",["Leave. I’m probably left out for a reason.","Try to join in when there’s an opportunity.","Interrupt and redirect the conversation."])
 	if cmd == 0
-		lonely =+ 2
-		bashful =+ 1
+		lonely += 2
+		bashful += 1
 	elsif cmd == 1
-		timid =+ 2
-		mild =+ 1
+		timid += 2
+		mild += 1
 	elsif cmd == 2
-		bold =+2
-		sassy =+1
+		bold +=2
+		sassy +=1
 	end
 
 	natures = [lonely,brave,relaxed,rash,bashful,careful,hardy,serious,naive,lax,gentle,quirky,bold,sassy,jolly,impish,docile,naughty,calm,quiet,adamant,mild,modest,hasty,timid]
 
-	playernature = natures.index(natures.max)+1
+	count = 0
+	natures.each do | num |
+		if num.equal? natures.max
+			count += 1
+		end
+	end
 
-	p natures.max
+	if count > 1 # If there's a tie
+		tiebreaker = Array.new
+		natures.each_with_index do | element, index |
+			if element.equal? natures.max
+				tiebreaker << index
+			end
+	end
+	 	playernature = tiebreaker.sample
+	else
+		playernature = natures.index(natures.max)+1
+	end
 
-	natures.each_index.select{|i| natures[i] == natures.max}
-	p natures.each_index.select{|i| natures[i] == natures.max}
 
 	if playernature == 1
-		pbMessage("ur sad heres a baby")
 		pbAddPokemon(:BULBASAUR,5)
 	elsif playernature == 2
 		pbAddPokemon(:CHARMANDER,5)
