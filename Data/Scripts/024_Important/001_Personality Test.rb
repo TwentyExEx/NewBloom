9f693127bda354852503b716cc17f74870ab28a2
James <james.magoffin@my.jcu.edu.au>
2020-11-26 14:35:33 +1000

    Update 001_Personality Test.rb
    Updated the Personality test with the remainder of the questions.

--- Data/Scripts/024_Important/001_Personality Test.rb
+++ Data/Scripts/024_Important/001_Personality Test.rb
@@ -35,14 +35,13 @@ def pbPersonalityTest
 		quirky += 1
 	end
 # Question 2
-	cmd=pbMessage("Your friend takes a spectacular fall! What do you do?",["Follow after them and help them up.","Call for medical assistance.","Point and laugh. How did they manage that?!"])
+	cmd=pbMessage("You’ve just seen someone trip! What do you do?",["Go over to them and help them up.","Take them to the hospital. Better safe than sorry!","Quietly laugh. How did they manage that?!"])
 	if cmd == 0
 		mild += 2
 		brave += 1
 	elsif cmd == 1
 		hasty += 2
 		careful += 1
-		p careful
 	elsif cmd == 2
 		naughty +=2
 		impish +=1
@@ -70,6 +69,198 @@ def pbPersonalityTest
 	elsif cmd == 2
 		bold +=2
 		sassy +=1
+	end
+# Question 5
+	cmd=pbMessage("You find something at a great bargain! What do you do?",["Jump at the deal and buy it.","Consider if I really need it before deciding."])
+	if cmd == 0
+		hasty += 2
+		adamant += 1
+	elsif cmd == 1
+		careful += 2
+		mild += 1
+	end
+# Question 6
+	cmd=pbMessage("You’re going on a day trip to the mountains. What are you most excited to do?",["Hiking. It’s perfect to clear my mind.","Roasting marshmallows. Delicious!","Telling ghost stories. Boo!"])
+	if cmd == 0
+		serious += 2
+		hardy += 1
+	elsif cmd == 1
+		quirky += 2
+		relaxed += 1
+	elsif cmd == 2
+		impish +=2
+		jolly +=1
+	end
+# Question 7
+	cmd=pbMessage("You are planning a vacation! Where would you prefer to go?",["Somewhere totally new.","Somewhere close to home."])
+	if cmd == 0
+		bold += 2
+		brave += 1
+	elsif cmd == 1
+		timid += 2
+		quiet += 1
+	end
+# Question 8
+	cmd=pbMessage("If you face a problem at work, what is the first thing you do?",["Pretend I never noticed.","Try a bunch of different things to see what fixes it.","Ask for help. Someone else is better suited for this."])
+	if cmd == 0
+		naughty += 2
+		hasty += 1
+	elsif cmd == 1
+		sassy += 2
+		careful += 1
+	elsif cmd == 2
+		modest +=2
+		docile +=1
+	end
+# Question 9
+	cmd=pbMessage("You are at a restaurant you’ve been to before. What do you order?",["Play it safe and stick with my usual order.","Look through the menu and pick what sounds good.","Go with the waiter’s recommendation, or whatever is a best seller."])
+	if cmd == 0
+		adamant += 2
+		modest += 1
+	elsif cmd == 1
+		jolly += 2
+		lax += 1
+	elsif cmd == 2
+		docile +=2
+		calm +=1
+	end
+# Question 10
+	cmd=pbMessage("How do you go about making big decisions?",["Go with my gut then move on.","Weigh out the pros and cons then go with the most logical option."])
+	if cmd == 0
+		rash += 2
+		adamant += 1
+	elsif cmd == 1
+		serious += 2
+		modest += 1
+	end
+# Question 11
+	cmd=pbMessage("How do you usually participate in group projects?",["Assume the role of the leader.","Do whatever no one else wants to do.","Act as moral support."])
+	if cmd == 0
+		hardy += 2
+		rash += 1
+	elsif cmd == 1
+		docile += 2
+		lonely += 1
+	elsif cmd == 2
+		naive +=2
+		gentle +=1
+	end
+# Question 12
+	cmd=pbMessage("Your friend just got a new pet! What do you do?",["Carefully try to pet it.","Let it adjust to me before approaching it.","Try to pick it up and pet it."])
+	if cmd == 0
+		gentle += 2
+		timid += 1
+	elsif cmd == 1
+		calm += 2
+		relaxed += 1
+	elsif cmd == 2
+		rash +=2
+		bold +=1
+	end
+# Question 13
+	cmd=pbMessage("What is your workflow like when doing tasks?",["Work hard until the task is complete.","Accomplish it little by little in short bursts.","Procrastinate until the last minute, then rush to finish it."])
+	if cmd == 0
+		hardy += 2
+		serious += 1
+	elsif cmd == 1
+		relaxed += 2
+		naive += 1
+	elsif cmd == 2
+		lax +=2
+		naughty +=1
+	end
+# Question 14
+	cmd=pbMessage("You have a crush on someone in your class. What do you do?",["Share my lunch with them. Everyone loves food!","Never tell them. They probably don’t feel the same, anyway.","Write them a letter directly telling them how I feel."])
+	if cmd == 0
+		naive += 2
+		quirky += 1
+	elsif cmd == 1
+		bashful += 2
+		lonely += 1
+	elsif cmd == 2
+		brave +=2
+		gentle +=1
+	end
+# Question 15
+	cmd=pbMessage("What do you spend more of your time thinking about?",["How things are.","How things should be.","What I should have for dinner."])
+	if cmd == 0
+		adamant += 2
+		hardy += 1
+	elsif cmd == 1
+		mild += 2
+		quiet += 1
+	elsif cmd == 2
+		quirky +=2
+		naive +=1
+	end
+# Question 16
+	cmd=pbMessage("The people at the next table are singing for someone's birthday. What do you do?",["Join in. The more the merrier!","Stay silent. None of my business."])
+	if cmd == 0
+		jolly += 2
+		hasty += 1
+	elsif cmd == 1
+		quiet += 2
+		bashful += 1
+	end
+# Question 17
+	cmd=pbMessage("Your friend hasn’t replied to your texts in a while. What do you think?",["They’re probably just busy.","I must have done something that upset them."])
+	if cmd == 0
+		relaxed += 2
+		sassy += 1
+	elsif cmd == 1
+		lonely += 2
+		impish += 1
+	end
+# Question 18
+	cmd=pbMessage("You make a joke and nobody hears it, but someone else repeats it and everyone laughs. What do you do?",["Laugh along. It’s no big deal.","Tell everyone you said it first."])
+	if cmd == 0
+		calm += 2
+		lax += 1
+	elsif cmd == 1
+		impish += 2
+		rash += 1
+	end
+# Question 19
+	cmd=pbMessage("Your friend has painted some art! It’s not very good. How do you react?",["Tell them the truth. No point lying to them.","Avoid commenting. Can’t lie if I don’t talk.","Tell them it looks great! I don’t want to discourage them."])
+	if cmd == 0
+		brave += 2
+		bold += 1
+	elsif cmd == 1
+		quiet += 2
+		naughty += 1
+	elsif cmd == 2
+		gentle +=2
+		docile +=1
+	end
+# Question 20
+	cmd=pbMessage("There’s one last slot in your Pokémon team. Describe what kind of Pokémon you’d give that spot to.",["Edgy and intimidating","Majestic and beautiful","Smart and cunning"])
+	if cmd == 0
+		lonely += 1
+		careful += 1
+		naive += 1
+		bold += 1
+		impish += 1
+		docile += 1
+		adamant += 1
+		modest += 1
+	elsif cmd == 1
+		brave += 1
+		bashful += 1
+		lax += 1
+		gentle += 1
+		jolly += 1
+		naughty += 1
+		mild += 1
+		hasty += 1
+	elsif cmd == 2
+		relaxed +=1
+		hardy +=1
+		serious += 1
+		quirky += 1
+		sassy += 1
+		calm += 1
+		quiet += 1
+		timid += 1
 	end
 
 	natures = [lonely,brave,relaxed,rash,bashful,careful,hardy,serious,naive,lax,gentle,quirky,bold,sassy,jolly,impish,docile,naughty,calm,quiet,adamant,mild,modest,hasty,timid]
