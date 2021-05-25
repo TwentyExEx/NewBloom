712ba3c5149b938ac49f4790c30c3faa2ebe9dcc
kaistella <59009017+kaistella@users.noreply.github.com>
2021-05-25 20:19:15 +0800

    character customization setup attempt wip

--- Data/Scripts/001_Technical/001_MKXP_Compatibility.rb
+++ Data/Scripts/001_Technical/001_MKXP_Compatibility.rb
@@ -21,58 +21,6 @@ module Graphics
     return self.delta.to_f / 1_000_000
   end
 
-    def saveToPng(filename)
-    bytes=[
-       0x89,0x50,0x4E,0x47,0x0D,0x0A,0x1A,0x0A,0x00,0x00,0x00,0x0D
-    ].pack("CCCCCCCCCCCC")
-    ihdr=[
-       0x49,0x48,0x44,0x52,swap32(self.width),swap32(self.height),
-       0x08,0x06,0x00,0x00,0x00
-    ].pack("CCCCVVCCCCC")
-    crc=Zlib::crc32(ihdr)
-    ihdr+=[swap32(crc)].pack("V")
-    bytesPerScan=self.width*4
-    row=(self.height-1)*bytesPerScan
-    data=self.getData
-    data2=data.clone
-    x=""
-    len=bytesPerScan*self.height
-    ttt=Time.now
-    if SwapRgb
-      SwapRgb.call(data2,data2.length)
-    else
-      # the following is considerably slower
-      b=0; c=2
-      while b!=len
-        data2[b]=data[c]
-        data2[c]=data[b]
-        b+=4; c+=4;
-      end
-    end
-#    $times.push(Time.now-ttt)
-    filter="\0"
-    while row>=0
-      thisRow=data2[row,bytesPerScan]
-      x.concat(filter)
-      x.concat(thisRow)
-      row-=bytesPerScan
-    end
-    x=Zlib::Deflate.deflate(x)
-    length=x.length
-    x="IDAT"+x
-    crc=Zlib::crc32(x)
-    idat=[swap32(length)].pack("V")
-    idat.concat(x)
-    idat.concat([swap32(crc)].pack("V"))
-    idat.concat([0,0x49,0x45,0x4E,0x44,0xAE,0x42,0x60,0x82].pack("VCCCCCCCC"))
-    File.open(filename,"wb") { |f|
-      f.write(bytes)
-      f.write(ihdr)
-      f.write(idat)
-    }
-  end
-end
-
 def pbSetResizeFactor(factor)
   if !$ResizeInitialized
     Graphics.resize_screen(Settings::SCREEN_WIDTH, Settings::SCREEN_HEIGHT)
